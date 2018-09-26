713bd3fe8a9efd9a80d71a39beeba4e6a045f6bb
Aakash <aakash.ohri19@gmail.com>
2018-09-26 13:40:33 +0530

    rxjs subs with timer and mat button

--- src/app/training/components/current-training/current-training.component.ts
+++ src/app/training/components/current-training/current-training.component.ts
@@ -1,28 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { map, shareReplay, take } from 'rxjs/operators';
-import { interval, Observable } from 'rxjs';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { map, shareReplay, take, takeUntil } from 'rxjs/operators';
+import { interval, Observable, fromEvent, concat } from 'rxjs';
 
 @Component({
   selector: 'app-current-training',
   templateUrl: './current-training.component.html',
   styleUrls: ['./current-training.component.css']
 })
-export class CurrentTrainingComponent implements OnInit {
+export class CurrentTrainingComponent implements OnInit, AfterViewInit {
   
   progressCount: number = 0;
   progress$: Observable<number>;
+  stop$: Observable<any>;
+  
+  @ViewChild('stop') private stopButton: any;
 
   constructor() { }
 
   ngOnInit() {
-    this.initProgress();
+    //this.initProgress();
+  }
+
+  ngAfterViewInit() {
+    
+   
+
+    this.stop$ = concat(fromEvent(this.stopButton._elementRef.nativeElement, 'click'), interval(1000).pipe(take(20)))
+
+    
+
+    const interval$: Observable<number> = interval(1000);
+    this.progress$ = interval$.pipe(
+      map(_ => this.progressCount+=5),
+      take(20),
+      takeUntil(this.stop$),
+      shareReplay()
+    )
   }
 
   private initProgress() {
     const interval$: Observable<number> = interval(1000);
     this.progress$ = interval$.pipe(
       map(_ => this.progressCount+=5),
-      take(20),
+      takeUntil(this.stop$),
       shareReplay()
     )
    
